bd4bc3d26f274cb00ce989e58dd6a288d448e628
DIOgoMateusClz <DDDragon71@gmail.com>
2024-07-12 20:44:25 -0300

    Update LocalizacaoActivity.cs
    1. Diogo

--- HealthVisualization/Activities/LocalizacaoActivity.cs
+++ HealthVisualization/Activities/LocalizacaoActivity.cs
@@ -64,7 +64,7 @@ namespace HealthVisualization.Activities
                         else
                         {
                             // Permissão negada, você pode mostrar uma mensagem ou fazer outra ação
-                            Toast.MakeText(this, "Permissão de localização foi negada.", ToastLength.Short).Show(); // TODO: Definir uma nova mensagem toast
+                            Toast.MakeText(this, "Não deu boa!", ToastLength.Short).Show(); // TODO: Definir uma nova mensagem toast
                         }
                     }
                     break;
