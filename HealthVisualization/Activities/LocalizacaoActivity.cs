3ed81abf38f7ea1915cd8e0f9a93a001a2b0f57e
Eugenio <genioduszeiko123@gmail.com>
2024-07-12 21:43:48 -0300

    Update LocalizacaoActivity.cs

--- HealthVisualization/Activities/LocalizacaoActivity.cs
+++ HealthVisualization/Activities/LocalizacaoActivity.cs
@@ -147,7 +147,7 @@ namespace HealthVisualization.Activities
             }
             else
             {
-                Toast.MakeText(this, "GPS is not enabled", ToastLength.Short).Show(); // TODO: Definir uma nova mensagem toast
+                Toast.MakeText(this, "GPS não esta disponivel", ToastLength.Short).Show();
             }
         }
 
